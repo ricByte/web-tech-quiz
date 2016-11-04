2bc651c459f958f1a5cba23aedee23fa3a6d4b12
ricByte <rvecchi@byte-code.com>
2016-11-04 07:08:18 +0100

    Create connector for DB : first step

---
+++ src/main/java/database/Connect.java
@@ -0,0 +1,74 @@
+package database;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+/**
+ * Servlet implementation class Connect
+ */
+@WebServlet("/Connect")
+public class Connect extends HttpServlet {
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public Connect() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+    /**
+     * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doGet(HttpServletRequest request,
+                         HttpServletResponse response) throws ServletException, IOException {
+
+        PrintWriter out = response.getWriter();
+
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            out.println("Can't load database driver");
+            //e.printStackTrace();
+            String strClassPath = System.getProperty("java.class.path");
+
+            System.out.println("Classpath is " + strClassPath);
+            return;
+        }
+
+        Connection conn = null;
+
+        try {
+            conn = DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/webTech", "root", "");
+        } catch (SQLException e) {
+            out.println("Can't connect to database.");
+            return;
+        }
+
+        out.println("Connected to database.");
+
+        try {
+            conn.close();
+            out.println("Logged out from database...");
+        } catch (SQLException e) {
+            out.println("Can't logged out");
+        }
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        // TODO Auto-generated method stub
+    }
+
+}
