d04e7e5e93e4d2311b51d1774a188779827e5ab6
Riccardo Vecchi <rvecchi@byte-code.com>
2017-02-02 18:13:04 +0100

    Add method for generateRandomNumber

--- src/main/java/services/utils/RandomAlphaNum.java
+++ src/main/java/services/utils/RandomAlphaNum.java
@@ -1,5 +1,6 @@
 package services.utils;
 
+import java.util.Random;
 import java.util.UUID;
 
 public class RandomAlphaNum {
@@ -7,5 +8,19 @@ public class RandomAlphaNum {
     public static String generateId() {
         UUID uniqueKey = UUID.randomUUID();
         return uniqueKey.toString();
+    }
+
+    public static int generateNumber(int aStart, int aEnd) {
+
+        Random random = new Random();
+        if (aStart > aEnd) {
+            throw new IllegalArgumentException("Start cannot exceed End.");
+        }
+        //get the range, casting to long to avoid overflow problems
+        long range = (long)aEnd - (long)aStart + 1;
+        // compute a fraction of the range, 0 <= frac < range
+        long fraction = (long)(range * random.nextDouble());
+
+        return (int)(fraction + aStart);
     }
 }
